79fd70ddaf3f5204054867ff467358a04f44b91a
Er-Kal <75757517+Er-Kal@users.noreply.github.com>
2025-10-07 18:28:53 +0100

    Makes service role key not public

--- tech-blog/src/utils/supabase/adminClient.ts
+++ tech-blog/src/utils/supabase/adminClient.ts
@@ -5,6 +5,6 @@ import { createClient } from "@supabase/supabase-js";
 export async function createAdminClient(){
     return createClient(
         process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!
+        process.env.NEXT_PRIVATE_SUPABASE_SERVICE_ROLE_KEY!
     )
 }
