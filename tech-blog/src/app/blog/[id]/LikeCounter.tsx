8fbe95cfd190eeba4d5cf6080488823e46b01c42
Er-Kal <75757517+Er-Kal@users.noreply.github.com>
2025-10-17 11:54:12 +0100

    Fully functional like button

--- tech-blog/src/app/blog/[id]/LikeCounter.tsx
+++ tech-blog/src/app/blog/[id]/LikeCounter.tsx
@@ -1,15 +1,48 @@
-import { FaRegThumbsUp } from 'react-icons/fa';
-import styles from './blog.module.css'
+"use client";
 
+import { FaRegThumbsUp } from "react-icons/fa";
+import styles from "./blog.module.css";
+import { useState, useEffect } from "react";
+import { getLikes, likeBlog } from "./actions";
+import { createClient } from "@/utils/supabase/client";
+import { User } from "@supabase/supabase-js";
 
 type Props = {
-    id:number;
-}
+	id: number;
+};
 
-export default function LikeCounter(props:Props){
-    return (<div className={styles.likeContainer}>
-        <FaRegThumbsUp/>
-        <p>1</p>
-        <button>Like</button>
-    </div>)
+export default function LikeCounter(props: Props) {
+	const [likes, setLikes] = useState<any>("");
+    const [user, setUser] = useState<User | null>();
+    const supabase = createClient();
+
+    useEffect(() =>{
+        const getUser = async () =>{
+            const {data:{user}} = await supabase.auth.getUser();
+            setUser(user);
+        }
+        getUser();
+    })
+
+	const getLikeCount = async () => {
+		const amount = await getLikes(props.id);
+		setLikes(amount);
+	};
+
+	useEffect(() => {
+		getLikeCount();
+	});
+
+	const likeClicked = () => {
+		likeBlog(props.id);
+        getLikeCount();
+	};
+
+	return (
+		<div className={styles.likeContainer}>
+			<FaRegThumbsUp />
+			<p>{likes} </p>
+            {user && <button onClick={likeClicked}>Like</button>}
+		</div>
+	);
 }
--- tech-blog/src/app/blog/[id]/actions.ts
+++ tech-blog/src/app/blog/[id]/actions.ts
@@ -1,7 +1,45 @@
 import { createClient } from "@/utils/supabase/client";
 
-export async function likeBlog(blogId:number){
-    const supabase = await createClient();
-    
-    const {error:likeError} = await supabase.from('bloglikes').insert({blogId});
+export async function likeBlog(blogId: number) {
+	const supabase = await createClient();
+	const {
+		data: { user },
+	} = await supabase.auth.getUser();
+
+	if (!user) {
+		return;
+	}
+
+	const { data: likeFound } = await supabase
+		.from("bloglikes")
+		.select("*")
+		.eq("blogid", blogId)
+		.eq("user_id", user.id);
+
+	if (likeFound!.length>0) {
+		await supabase
+			.from("bloglikes")
+			.delete()
+			.eq("blogid", blogId)
+			.eq("user_id", user.id);
+		return;
+	}
+
+	await supabase.from("bloglikes").insert({ blogid: blogId });
+
+	return;
+}
+
+export async function getLikes(blogId: number) {
+	const supabase = await createClient();
+
+	const { data: count } = await supabase
+		.from("bloglikes")
+		.select("*", { count: "exact" })
+		.eq("blogid", blogId);
+
+	if (!count) {
+		return 0;
+	}
+	return count.length;
 }
